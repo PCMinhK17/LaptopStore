8f66e7a395b721567200902feab6d7957a617a82
PCMinK17 <minhpche172291@fpt.edu.vn>
2026-01-21 17:36:41 +0700

    Sửa lại front-end cho màn hình add product, chỉnh sửa lặt vặt

--- LaptopStore/Controllers/ProductManagementController.cs
+++ LaptopStore/Controllers/ProductManagementController.cs
@@ -1,4 +1,5 @@
-﻿using LaptopStore.Models;
+﻿using LaptopStore.DTOs.ProductDTOs;
+using LaptopStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,12 +7,31 @@ namespace LaptopStore.Controllers;
 
 public class ProductManagementController : Controller
 {
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public ProductManagementController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
     public IActionResult Index()
     {
         using (LaptopStoreDbContext context = new LaptopStoreDbContext())
         {
             var products = context.Products.Include(p => p.Category).Include(p => p.Brand).Include(p => p.ProductImages).ToList();
-            return View("~/Views/Manager/ProductManagement.cshtml", products);
+            var productList = products.Select(p => new ProductResponse
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Sku = p.Sku,
+                BrandName = p.Brand.Name,
+                CategoryName = p.Category.Name,
+                Price = p.Price,
+                StockQuantity = p.StockQuantity,
+                ProductImages = p.ProductImages,
+                IsActive = p.IsActive
+            }).ToList();
+            return View("~/Views/Manager/ProductManagement.cshtml", productList);
         }
     }
 
@@ -28,10 +48,18 @@ public class ProductManagementController : Controller
     }
 
     [HttpPost]
-    public IActionResult AddNewProduct(Product newProduct)
+    public async Task<IActionResult> AddNewProduct(AddNewProductRequest newProduct)
     {
         using (LaptopStoreDbContext context = new LaptopStoreDbContext())
         {
+            if (decimal.TryParse(newProduct.Price, out var price))
+            {
+                if (price < 1000 || price > 100000000)
+                {
+                    ModelState.AddModelError("Price", "Giá phải nằm trong khoảng 1000 đến 100000000");
+                }
+            }
+
             if (!ModelState.IsValid)
             {
 
@@ -41,8 +69,55 @@ public class ProductManagementController : Controller
                 return View("~/Views/Manager/AddNewProduct.cshtml", newProduct);
             }
 
-            context.Products.Add(newProduct);
-            context.SaveChanges();
+            await context.Products.AddAsync(new Product
+            {
+                Name = newProduct.Name,
+                Sku = newProduct.Sku,
+                BrandId = newProduct.BrandId,
+                CategoryId = newProduct.CategoryId,
+                Description = newProduct.Description,
+                ShortDescription = newProduct.ShortDescription,
+                Price = price,
+                Cpu = newProduct.Cpu,
+                Ram = newProduct.Ram,
+                HardDrive = newProduct.HardDrive,
+                Gpu = newProduct.Gpu,
+                ScreenSize = newProduct.ScreenSize,
+                Weight = newProduct.Weight
+            });
+
+            await context.SaveChangesAsync();
+
+            var product = await context.Products.OrderByDescending(p => p.Id).FirstOrDefaultAsync();
+
+            //Lưu file ảnh
+            string folder = "images\\";
+            string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folder);
+            Console.WriteLine(serverFolder);
+
+            string fileName = Guid.NewGuid().ToString() + "_" + newProduct.ImageFile?.FileName;
+            string filePath = Path.Combine(serverFolder, fileName);
+
+            Console.WriteLine(filePath);
+
+            if (!Directory.Exists(serverFolder))
+            {
+                Directory.CreateDirectory(serverFolder);
+            }
+
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                await newProduct.ImageFile.CopyToAsync(fileStream);
+            }
+
+            context.ProductImages.Add(new ProductImage
+            {
+                ProductId = product.Id,
+                ImageUrl = Path.Combine(folder, fileName),
+                IsThumbnail = true
+            });
+
+            await context.SaveChangesAsync();
 
             return RedirectToAction("Index");
         }
---
+++ LaptopStore/DTOs/ProductDTOs/AddNewProductRequest.cs
@@ -0,0 +1,50 @@
+﻿using LaptopStore.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace LaptopStore.DTOs.ProductDTOs;
+
+public class AddNewProductRequest
+{
+
+    //[Required(ErrorMessage = "Name can't be empty")]
+    //[StringLength(99, ErrorMessage = "Name must have under 100 characters")]
+    [Required(ErrorMessage = "Vui lòng điền tên cho laptop")]
+    [StringLength(100, ErrorMessage = "Tên không được dài quá 100 ký tự")]
+    public string Name { get; set; } = null!;
+
+    [Required(ErrorMessage = "Vui lòng nhập Mã vạch cho laptop")]
+    public string? Sku { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng chọn phân loại cho laptop")]
+    public int? CategoryId { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng chọn thương hiệu cho laptop")]
+    public int? BrandId { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng chọn thương hiệu cho laptop")]
+    public string? Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? ShortDescription { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng nhập CPU cho laptop")]
+    public string? Cpu { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng nhập RAM cho laptop")]
+    public string? Ram { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng nhập Ổ cứng cho laptop")]
+    public string? HardDrive { get; set; }
+
+    public string? Gpu { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng nhập Kích cỡ màn hình cho laptop")]
+    public string? ScreenSize { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng nhập Cân nặng cho laptop")]
+    public string? Weight { get; set; }
+
+    [Required(ErrorMessage = "Vui lòng tải ảnh thumbnail cho laptop")]
+    public IFormFile? ImageFile { get; set; }
+}
