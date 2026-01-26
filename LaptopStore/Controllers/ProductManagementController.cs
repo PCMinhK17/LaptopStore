e97b2b429c5f5e7798bb286bd28051fa5e314152
PCMinK17 <minhpche172291@fpt.edu.vn>
2026-01-26 22:36:24 +0700

    Add field verification for Add new Product

--- LaptopStore/Controllers/ProductManagementController.cs
+++ LaptopStore/Controllers/ProductManagementController.cs
@@ -2,147 +2,138 @@
 using LaptopStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace LaptopStore.Controllers;
 
 public class ProductManagementController : Controller
 {
     private readonly IWebHostEnvironment _webHostEnvironment;
+    private readonly LaptopStoreDbContext _context = new LaptopStoreDbContext();
 
-    public ProductManagementController(IWebHostEnvironment webHostEnvironment)
+    public ProductManagementController(IWebHostEnvironment webHostEnvironment, LaptopStoreDbContext context)
     {
         _webHostEnvironment = webHostEnvironment;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        using (LaptopStoreDbContext context = new LaptopStoreDbContext())
+        var products = _context.Products.Include(p => p.Category).Include(p => p.Brand).Include(p => p.ProductImages).ToList();
+        var productList = products.Select(p => new ProductResponse
         {
-            var products = context.Products.Include(p => p.Category).Include(p => p.Brand).Include(p => p.ProductImages).ToList();
-            var productList = products.Select(p => new ProductResponse
-            {
-                Id = p.Id,
-                Name = p.Name,
-                Sku = p.Sku,
-                BrandName = p.Brand.Name,
-                CategoryName = p.Category.Name,
-                Price = p.Price,
-                StockQuantity = p.StockQuantity,
-                Cpu = p.Cpu,
-                Ram = p.Ram,
-                HardDrive = p.HardDrive,
-                Gpu = p.Gpu,
-                ScreenSize = p.ScreenSize,
-                Weight = p.Weight,
-                ProductImages = p.ProductImages,
-                IsActive = p.IsActive
-            }).ToList();
-            return View("~/Views/Manager/ProductManagement.cshtml", productList);
-        }
+            Id = p.Id,
+            Name = p.Name,
+            Sku = p.Sku,
+            BrandName = p.Brand.Name,
+            CategoryName = p.Category.Name,
+            Price = p.Price,
+            StockQuantity = p.StockQuantity,
+            Cpu = p.Cpu,
+            Ram = p.Ram,
+            HardDrive = p.HardDrive,
+            Gpu = p.Gpu,
+            ScreenSize = p.ScreenSize,
+            Weight = p.Weight,
+            ProductImages = p.ProductImages,
+            IsActive = p.IsActive
+        }).ToList();
+        return View("~/Views/Manager/ProductManagement.cshtml", productList);
     }
 
     [HttpGet]
     public IActionResult AddNewProduct()
     {
-        using (LaptopStoreDbContext context = new LaptopStoreDbContext())
-        {
-            ViewBag.Brands = context.Brands.ToList();
-            ViewBag.Categories = context.Categories.ToList();
+        ViewBag.Brands = _context.Brands.ToList();
+        ViewBag.Categories = _context.Categories.ToList();
 
-            return View("~/Views/Manager/AddNewProduct.cshtml");
-        }
+        return View("~/Views/Manager/AddNewProduct.cshtml");
     }
 
     [HttpPost]
     public async Task<IActionResult> AddNewProduct(AddNewProductRequest newProduct)
     {
-        using (LaptopStoreDbContext context = new LaptopStoreDbContext())
+        if (decimal.TryParse(newProduct.Price, out var price))
         {
-            if (decimal.TryParse(newProduct.Price, out var price))
+            if (price < 1000 || price > 1000000000)
             {
-                if (price < 1000 || price > 100000000)
-                {
-                    ModelState.AddModelError("Price", "Giá phải nằm trong khoảng 1000 đến 100000000");
-                }
+                ModelState.AddModelError("Price", "Giá phải nằm trong khoảng 1000 đến 1000000000");
             }
-
-            if (!ModelState.IsValid)
-            {
-
-                ViewBag.Brands = context.Brands.ToList();
-                ViewBag.Categories = context.Categories.ToList();
-
-                return View("~/Views/Manager/AddNewProduct.cshtml", newProduct);
-            }
-
-            await context.Products.AddAsync(new Product
-            {
-                Name = newProduct.Name,
-                Sku = newProduct.Sku,
-                BrandId = newProduct.BrandId,
-                CategoryId = newProduct.CategoryId,
-                Description = newProduct.Description,
-                ShortDescription = newProduct.ShortDescription,
-                Price = price,
-                Cpu = newProduct.Cpu,
-                Ram = newProduct.Ram,
-                HardDrive = newProduct.HardDrive,
-                Gpu = newProduct.Gpu,
-                ScreenSize = newProduct.ScreenSize,
-                Weight = newProduct.Weight
-            });
-
-            await context.SaveChangesAsync();
-
-            var product = await context.Products.OrderByDescending(p => p.Id).FirstOrDefaultAsync();
-
-            //Lưu file ảnh
-            string folder = "images\\";
-            string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folder);
-            Console.WriteLine(serverFolder);
-
-            string fileName = Guid.NewGuid().ToString() + "_" + newProduct.ImageFile?.FileName;
-            string filePath = Path.Combine(serverFolder, fileName);
-
-            Console.WriteLine(filePath);
-
-            if (!Directory.Exists(serverFolder))
-            {
-                Directory.CreateDirectory(serverFolder);
-            }
-
-            using (var fileStream = new FileStream(filePath, FileMode.Create))
-            {
-                await newProduct.ImageFile.CopyToAsync(fileStream);
-            }
-
-            context.ProductImages.Add(new ProductImage
-            {
-                ProductId = product.Id,
-                ImageUrl = Path.Combine(folder, fileName),
-                IsThumbnail = true
-            });
-
-            await context.SaveChangesAsync();
-
-            return RedirectToAction("Index");
         }
+
+        if (!ModelState.IsValid)
+        {
+
+            ViewBag.Brands = _context.Brands.ToList();
+            ViewBag.Categories = _context.Categories.ToList();
+
+            return View("~/Views/Manager/AddNewProduct.cshtml", newProduct);
+        }
+
+        await _context.Products.AddAsync(new Product
+        {
+            Name = newProduct.Name,
+            Sku = newProduct.Sku,
+            BrandId = newProduct.BrandId,
+            CategoryId = newProduct.CategoryId,
+            Description = newProduct.Description,
+            ShortDescription = newProduct.ShortDescription,
+            Price = price,
+            Cpu = newProduct.Cpu,
+            Ram = newProduct.Ram,
+            HardDrive = newProduct.HardDrive,
+            Gpu = newProduct.Gpu,
+            ScreenSize = newProduct.ScreenSize,
+            Weight = newProduct.Weight
+        });
+
+        await _context.SaveChangesAsync();
+
+        var product = await _context.Products.OrderByDescending(p => p.Id).FirstOrDefaultAsync();
+
+        //Lưu file ảnh
+        string folder = "images\\";
+        string serverFolder = Path.Combine(_webHostEnvironment.WebRootPath, folder);
+        Console.WriteLine(serverFolder);
+
+        string fileName = Guid.NewGuid().ToString() + "_" + newProduct.ImageFile?.FileName;
+        string filePath = Path.Combine(serverFolder, fileName);
+
+        Console.WriteLine(filePath);
+
+        if (!Directory.Exists(serverFolder))
+        {
+            Directory.CreateDirectory(serverFolder);
+        }
+
+        using (var fileStream = new FileStream(filePath, FileMode.Create))
+        {
+            await newProduct.ImageFile.CopyToAsync(fileStream);
+        }
+
+        _context.ProductImages.Add(new ProductImage
+        {
+            ProductId = product.Id,
+            ImageUrl = Path.Combine(folder, fileName),
+            IsThumbnail = true
+        });
+
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction("Index");
     }
 
     [HttpGet]
     public IActionResult ProductDetails(int id)
     {
-        using (LaptopStoreDbContext context = new LaptopStoreDbContext())
-        {
-            var product = context.Products
+        var product = _context.Products
                 .Include(p => p.Brand)
                 .Include(p => p.Category)
                 .FirstOrDefault(p => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View("~/Views/Manager/ProductDetails.cshtml", product);
+        if (product == null)
+        {
+            return NotFound();
         }
+        return View("~/Views/Manager/ProductDetails.cshtml", product);
     }
 }
--- LaptopStore/DTOs/ProductDTOs/AddNewProductRequest.cs
+++ LaptopStore/DTOs/ProductDTOs/AddNewProductRequest.cs
@@ -9,10 +9,11 @@ public class AddNewProductRequest
     //[Required(ErrorMessage = "Name can't be empty")]
     //[StringLength(99, ErrorMessage = "Name must have under 100 characters")]
     [Required(ErrorMessage = "Vui lòng điền tên cho laptop")]
-    [StringLength(100, ErrorMessage = "Tên không được dài quá 100 ký tự")]
+    [StringLength(255, ErrorMessage = "Tên không được dài quá 255 ký tự")]
     public string Name { get; set; } = null!;
 
     [Required(ErrorMessage = "Vui lòng nhập Mã vạch cho laptop")]
+    [StringLength(50, ErrorMessage = "Mã SKU không được dài quá 255 ký tự")]
     public string? Sku { get; set; }
 
     [Required(ErrorMessage = "Vui lòng chọn phân loại cho laptop")]
@@ -26,23 +27,30 @@ public class AddNewProductRequest
 
     public string? Description { get; set; }
 
+    [StringLength(500, ErrorMessage = "Mô tả ngắn không được dài quá 500 ký tự")]
     public string? ShortDescription { get; set; }
 
     [Required(ErrorMessage = "Vui lòng nhập CPU cho laptop")]
+    [StringLength(100, ErrorMessage = "CPU không được dài quá 100 ký tự")]
     public string? Cpu { get; set; }
 
     [Required(ErrorMessage = "Vui lòng nhập RAM cho laptop")]
+    [StringLength(50, ErrorMessage = "RAM không được dài quá 50 ký tự")]
     public string? Ram { get; set; }
 
     [Required(ErrorMessage = "Vui lòng nhập Ổ cứng cho laptop")]
+    [StringLength(100, ErrorMessage = "Ổ cứng không được dài quá 100 ký tự")]
     public string? HardDrive { get; set; }
 
+    [StringLength(100, ErrorMessage = "Card đồ họa không được dài quá 100 ký tự")]
     public string? Gpu { get; set; }
 
     [Required(ErrorMessage = "Vui lòng nhập Kích cỡ màn hình cho laptop")]
+    [StringLength(50, ErrorMessage = "Kích thước màn hình không đưoợc dài quá 50 ký tự")]
     public string? ScreenSize { get; set; }
 
     [Required(ErrorMessage = "Vui lòng nhập Cân nặng cho laptop")]
+    [StringLength(50, ErrorMessage = "Cân nặng không được dài quá 50 ký tự")]
     public string? Weight { get; set; }
 
     [Required(ErrorMessage = "Vui lòng tải ảnh thumbnail cho laptop")]
