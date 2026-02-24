721b44b3e080025a595780c6ff9727185fb07553
huym1nh1 <minhthhe171134@fpt.edu.vn>
2026-02-24 21:45:48 +0700

    add cart + image manage
    add cart + image manage

--- LaptopStore/DTOs/ProductDTOs/UpdateProductRequest.cs
+++ LaptopStore/DTOs/ProductDTOs/UpdateProductRequest.cs
@@ -1,13 +1,12 @@
-using LaptopStore.Models;
 using System.ComponentModel.DataAnnotations;
 
 namespace LaptopStore.DTOs.ProductDTOs;
 
-public class AddNewProductRequest
+public class UpdateProductRequest
 {
+    [Required]
+    public int Id { get; set; }
 
-    //[Required(ErrorMessage = "Name can't be empty")]
-    //[StringLength(99, ErrorMessage = "Name must have under 100 characters")]
     [Required(ErrorMessage = "Vui lòng điền tên cho laptop")]
     [StringLength(100, ErrorMessage = "Tên không được dài quá 100 ký tự")]
     public string Name { get; set; } = null!;
@@ -21,7 +20,7 @@ public class AddNewProductRequest
     [Required(ErrorMessage = "Vui lòng chọn thương hiệu cho laptop")]
     public int? BrandId { get; set; }
 
-    [Required(ErrorMessage = "Vui lòng chọn thương hiệu cho laptop")]
+    [Required(ErrorMessage = "Vui lòng nhập giá cho laptop")]
     public string? Price { get; set; }
 
     public string? Description { get; set; }
@@ -45,6 +44,10 @@ public class AddNewProductRequest
     [Required(ErrorMessage = "Vui lòng nhập Cân nặng cho laptop")]
     public string? Weight { get; set; }
 
-    [Required(ErrorMessage = "Vui lòng tải ảnh thumbnail cho laptop")]
+    public int? StockQuantity { get; set; }
+
+    public bool IsActive { get; set; }
+
     public IFormFile? ImageFile { get; set; }
 }
+
