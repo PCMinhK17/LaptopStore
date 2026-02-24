0570aa11fc74677cd306cccf939c6e159ca6e858
PCMinK17 <minhpche172291@fpt.edu.vn>
2026-02-24 10:23:08 +0700

    Sửa lại nav bar cho admin

--- LaptopStore/Program.cs
+++ LaptopStore/Program.cs
@@ -65,6 +65,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
